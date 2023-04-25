4dfcf38bba429b79e162209319698e940302a49d
TenMayU <tennarakorn19@gmail.com>
2023-04-26 02:26:16 +0700

    add profile in navbar

--- flowerecom/src/pages/component/Navbar.js
+++ flowerecom/src/pages/component/Navbar.js
@@ -1,4 +1,5 @@
 import styles from'@/styles/Nav.module.css'
+import Image from 'next/image';
 
 
 export default function Navbar(){
@@ -6,13 +7,14 @@ export default function Navbar(){
         <>
         <nav className={styles.nav}>
             <h1 className={styles.navheadtext}>FLOWEECOM</h1>
+            <div className={styles.navmenu} >
             <div className={styles.navitem}>
-                  <li className={styles.navtext}>test</li>
-                  <li className={styles.navtext}>test</li>
-                  <li className={styles.navtext}>test</li>
-                  <li className={styles.navtext}>test</li>
-                
+                  <li className={styles.navtext}>test</li>             
             </div>
+               <div className={styles.navprofile}>
+                  <Image src="/icon.png" width={40} height={40} className={styles.imageprofile}></Image>
+              </div>
+            </div>      
         </nav>
         </>
     );
