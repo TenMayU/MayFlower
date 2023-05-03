5977cca2a8f70bc05de60c43979b01114a83452e
TenMayU <tennarakorn19@gmail.com>
2023-05-04 03:29:23 +0700

    respon grid list

--- flowerecom/src/pages/component/itemcategory/itemcard.js
+++ flowerecom/src/pages/component/itemcategory/itemcard.js
@@ -1,7 +1,9 @@
 import styles from "@/styles/item.module.css"
 import Image from "next/image"
+import { useRef, useState } from "react"
 export default function Itemcard({id}){
-
+const [test,settest] = useState('0')
+const reff = useRef('')
     return(
       <>
       <div className={styles.itemcard}>
@@ -9,10 +11,19 @@ export default function Itemcard({id}){
              <Image></Image>
         </div>
         <div className={styles.itemcard_name}>
-<h1>FLOWER</h1>
+          <input ref={reff} type="text"></input>
+          <button onClick={()=>{
+            settest(reff.current.value)
+          }} >test</button>
+ <p>{test}</p>
         </div>
         <div className={styles.itemcard_price}>       
         <h1>179B</h1>
+        </div>
+        <div className={styles.itemcard_tag}>
+         <p>#tag</p>
+         <p>#tag</p>
+         <p>#tag</p>
         </div>
       </div>
       </>
